8e295ad09734a650f48c9e37b23575da92ee5927
18616505009 <619627239@qq.com>
2020-12-24 17:22:55 +0800

    增加redis的list测试

--- redis-test/src/main/java/com/lmx/redistest/client/JedisService.java
+++ redis-test/src/main/java/com/lmx/redistest/client/JedisService.java
@@ -11,6 +11,7 @@ import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author lmx
@@ -68,6 +69,8 @@ public class JedisService implements ApplicationRunner {
         }
     }
 
+    /***hashmap操作***/
+
     public void hmset(String key, HashMap<String, String> map) {
         try (Jedis jedis = pool.getResource()) {
             jedis.hmset(key, map);
@@ -86,5 +89,122 @@ public class JedisService implements ApplicationRunner {
             return jedis.hmget(mapName, keys);
         }
     }
+
+    /**
+     * 查看指定名称的hashmap内字段数量
+     *
+     * @param mapName
+     */
+    public long hlen(String mapName) {
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.hlen(mapName);
+        }
+    }
+
+    /**
+     * 返回指定名称的hashmap内的所有key
+     *
+     * @param mapName
+     * @return
+     */
+    public Set<String> hkey(String mapName) {
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.hkeys(mapName);
+        }
+    }
+
+    /***redis列表操作***/
+
+    /**
+     * 在list头部增加一条数据
+     *
+     * @param listName
+     * @param value
+     */
+    public void lpush(String listName, String value) {
+        try (Jedis jedis = pool.getResource()) {
+            jedis.lpush("testList", value);
+        }
+    }
+
+    /**
+     * 在list头部增加一组数据
+     * 注意:lpush会依次在头部写入数据,因此list写入后与一般理解不太相同:
+     * eg lpush testList 1,2,3
+     * 实测testList左侧将会是 3,2,1
+     *
+     * @param listName
+     * @param values
+     */
+    public void lpush(String listName, String[] values) {
+        try (Jedis jedis = pool.getResource()) {
+            jedis.lpush(listName, values);
+        }
+    }
+
+    /**
+     * 在list尾部增加一条数据
+     *
+     * @param listName
+     * @param value
+     */
+    public void rpush(String listName, String value) {
+        try (Jedis jedis = pool.getResource()) {
+            jedis.rpush(listName, value);
+        }
+    }
+
+    /**
+     * 在list尾部增加一串数据
+     *
+     * @param listName
+     * @param values
+     */
+    public void rpush(String listName, String[] values) {
+        try (Jedis jedis = pool.getResource()) {
+            jedis.rpush(listName, values);
+        }
+    }
+
+    /**
+     * 获取list长度
+     *
+     * @param listName
+     * @return
+     */
+    public long llen(String listName) {
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.llen(listName);
+        }
+    }
+
+    /**
+     * 指定下标提取list内的元素
+     *
+     * @param listName
+     * @param index    redis的list下标支持负值，表示从后往前找,-1为最后一位
+     * @return
+     */
+    public String lindex(String listName, long index) {
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.lindex(listName, index);
+        }
+    }
+
+    /**
+     * 根据起始index、结束index提取list的子list
+     * 可以通过redis命令: lrange ${listname} 0 -1 提取到全部list
+     *
+     * @param listName
+     * @param start
+     * @param stop
+     * @return
+     */
+    public List<String> lrange(String listName, long start, long stop) {
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.lrange(listName, start, stop);
+        }
+    }
+
 
 }
--- redis-test/src/main/java/com/lmx/redistest/jedis/CrudDemo.java
+++ redis-test/src/main/java/com/lmx/redistest/jedis/CrudDemo.java
@@ -31,10 +31,12 @@ public class CrudDemo {
         jedisService.del("lmx");
         System.out.println("get key lmx->" + jedisService.get("lmx"));
 
+        /***key过期测试***/
         System.out.println("设置key lmx，10sec后过期");
         jedisService.set("lmx", "limengxiao");
         jedisService.setExipre("lmx", 10);
 
+        /***map测试***/
         System.out.println("存储map testMap");
         HashMap<String, String> testMap = new HashMap<>();
         testMap.put("testDate", "20201223");
@@ -44,6 +46,28 @@ public class CrudDemo {
 
         List<String> testMapValues = jedisService.hmget("testMap", new String[]{"testDate", "testProjectName"});
         System.out.println("map查询结果-->" + testMapValues.toString());
+
+        System.out.println("testMap内元素数量->" + jedisService.hlen("testMap"));
+        System.out.println("testMap内的健->" + jedisService.hkey("testMap").toString());
+
+        /***list测试***/
+        System.out.println("list测试,先尝试删除testList避免有残留数据");
+        jedisService.del("testList");
+
+        System.out.println("在testList尾部写入两次，分别为A,B,C 和 D");
+        jedisService.rpush("testList", new String[]{"A", "B", "C"});
+        jedisService.rpush("testList", "D");
+
+        System.out.println("在testList头部写入两次，分别为4 和 1,2,3");
+        jedisService.lpush("testList", "4");
+        jedisService.lpush("testList", new String[]{"1", "2", "3"});
+
+        System.out.println("提取testList第4个元素-->"
+                + jedisService.lindex("testList", 3));
+        System.out.println("提取testList的第3-5位->"
+                + jedisService.lrange("testList", 2, 4));
+        System.out.println("提取testList的全部内容->"
+                + jedisService.lrange("testList", 0, -1));
 
     }
 
