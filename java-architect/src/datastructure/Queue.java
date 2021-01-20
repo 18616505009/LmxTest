1a0c4724e26b444bc346b3afabd920ce132445d9
18616505009 <619627239@qq.com>
2021-01-20 17:53:15 +0800

    增加PriorityQueue（优先队列）测试 以及通过Comparator对PriorityQueue的元素排序

--- java-architect/src/datastructure/Queue.java
+++ java-architect/src/datastructure/Queue.java
@@ -1,10 +1,8 @@
 package datastructure;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @author lmx
@@ -18,55 +16,68 @@ import java.util.concurrent.Executors;
 public class Queue {
 
     public static void main(String[] args) {
-//        testPriorityQueue();
+        testPriorityQueue();
+//        testArrayBlockingQueue();
+//        testLinkedBlockingQueue();
 
-        testBasket();
     }
+
+    //TODO 完成LinkedBlcokingQueue测试
+    private static void testLinkedBlockingQueue() {
+        LinkedBlockingQueue<Object> linkedBlockingQueue = new LinkedBlockingQueue<>();
+    }
+
 
     /**
      * 测试一个非阻塞队列
      */
     public static void testPriorityQueue() {
-        PriorityQueue<Object> priorityQueue = new PriorityQueue();
+        //创建一个比较器,用于修改优先队列内部元素的排序方式->降序排序
+        Comparator<Object> comparator = new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return (int) o1 > (int) o2 ? -1 : 1;
+            }
+        };
+        //在优先队列的构造方法中指定排序比较器
+        PriorityQueue<Object> priorityQueue = new PriorityQueue(comparator);
+        priorityQueue.add(3);
         priorityQueue.add(1);
         priorityQueue.add(2);
-        priorityQueue.add(3);
 
         System.out.println("out -> " + priorityQueue.poll());
         System.out.println("out -> " + priorityQueue.poll());
         System.out.println("out -> " + priorityQueue.poll());
         System.out.println("out -> " + priorityQueue.poll());
+
     }
-
 
     /**
      * 定义装苹果的篮子
      */
     public static class Basket {
         // 篮子，能够容纳3个苹果
-        BlockingQueue<String> basket = new ArrayBlockingQueue<String>(3);
+        ArrayBlockingQueue<String> basket = new ArrayBlockingQueue<String>(3);
 
         // 生产苹果，放入篮子
-        public void produce() throws InterruptedException {
+        public int produce() throws InterruptedException {
             // put方法放入一个苹果，若basket满了，等到basket有位置
             basket.put("An apple");
+            return basket.size();
         }
 
         // 消费苹果，从篮子中取走
-        public String consume() throws InterruptedException {
+        public int consume() throws InterruptedException {
             // get方法取出一个苹果，若basket为空，等到basket有苹果为止
             String apple = basket.take();
-            return apple;
-        }
-
-        public int getAppleNumber() {
             return basket.size();
         }
+
 
     }
 
     //　测试方法
-    public static void testBasket() {
+    public static void testArrayBlockingQueue() {
         // 建立一个装苹果的篮子
         final Basket basket = new Basket();
         // 定义苹果生产者
@@ -75,16 +86,13 @@ public class Queue {
                 try {
                     while (true) {
                         // 生产苹果
-                        System.out.println("生产者准备生产苹果："
-                                + System.currentTimeMillis());
-                        basket.produce();
-                        System.out.println("生产者生产苹果完毕："
-                                + System.currentTimeMillis());
-                        System.out.println("生产完后有苹果：" + basket.getAppleNumber() + "个");
+                        int size = basket.produce();
+                        System.out.println("生产者刚刚生产了苹果->当前数量：" + size + "个");
                         // 休眠300ms
                         Thread.sleep(300);
                     }
                 } catch (InterruptedException ex) {
+                    System.out.println("生产异常->" + ex.toString());
                 }
             }
         }
@@ -94,16 +102,13 @@ public class Queue {
                 try {
                     while (true) {
                         // 消费苹果
-                        System.out.println("消费者准备消费苹果："
-                                + System.currentTimeMillis());
-                        basket.consume();
-                        System.out.println("消费者消费苹果完毕："
-                                + System.currentTimeMillis());
-                        System.out.println("消费完后有苹果：" + basket.getAppleNumber() + "个");
+                        int size = basket.consume();
+                        System.out.println("消费者刚刚消费了苹果->当前数量：" + size + "个");
                         // 休眠1000ms
                         Thread.sleep(1000);
                     }
                 } catch (InterruptedException ex) {
+                    System.out.println("消费异常->" + ex.toString());
                 }
             }
         }
